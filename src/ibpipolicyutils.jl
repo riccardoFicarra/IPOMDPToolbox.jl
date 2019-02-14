7ccf07047f1579899180f81ab9b6880a6fba524e
riccardoFicarra <riccardo.ficarra95@gmail.com>
2019-02-14 15:52:12 -0600

    Made Node constructor work

--- src/ibpipolicyutils.jl
+++ src/ibpipolicyutils.jl
@@ -9,30 +9,49 @@ Structure used for policy nodes.
 ActionDist specifies the probability of executing the action at the corresponding index in actions
 Edges stores the possible edges given action and observations obtained after executing the action.
 Right now the (any) operator for observation is not implemented, we just have entries for all possible values
+receives as parameters all possible actions and all possible observations
 """
-mutable struct Node{A, W}
+abstract type AbstractEdge
+#used to implement reciprocally nested structs until it gets fixed
+end
+struct Node{A, W, E <: AbstractEdge}
 	actions::Vector{A}
-	actionDist::Array{Float64}
-	edges::Dict{A, Dict{W, Array{Edge}}}
-	#constructor to get a node that points to himself for all actions and obs
-	Node{actions::Vector{A}, observations::Vector{W}} = (
-									n = new();
-									n.actions = actions
-									n.actionDist = ones(Float64, size(actions))
-									randact = actions[rand(1:end)]
+	actionDist::Vector{Float64}
+	edges::Dict{A, Dict{W, Vector{E}}}
+	alpha::Vector{Float64}
 
-								)
 end
 
 """
 Each edge structure contains the node to which it brings to, and the probability of taking that edge.
 """
-struct Edge
-	next::Node
-	probability:Float64
+struct Edge{A, W} <: AbstractEdge
+	next::Node{A, W, Edge}
+	probability::Float64
 end
 
+function Node(actions::Vector{A}, observations::Vector{W}) where {A, W}
+    return actions::Vector{A}, zeros(Float64, length(actions)), Dict{A, Dict{W, Vector{Edge}}}(), Vector{Float64}()
+end
 
-struct IBPIPolicy
-	nodes::Array{Node}
+#Node(actions::Vector{A}, observations::Vector{W})= Node(actions::Vector{A}, zeros(Float64, length(actions)), Dict{A, Dict{W, Vector{Edge}}}(), Vector{Float64}())
+
+
+function InitialNode(actions::Vector{A}, observations::Vector{W}) where {A, W}
+		n = Node(actions, observations)
+		randindex = rand(1:length(actions))
+		n.actionDist[randindex] = 1.0
+		obsdict = Dict{W, Vector{Edge}}()
+		for obs in observations
+			obsdict[obs] = [Edge(n, 1.0)]
+		end
+		n.edges[n.actions[randindex]] = obsdict
+end
+
+struct Controller{A, W}
+	nodes::Array{Node{A, W, Edge}}
+end
+
+struct IBPIPolicy{A, W}
+	controllers:Array{Controller{A, W}}
 end
