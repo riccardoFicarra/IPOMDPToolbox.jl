9e4eb97844ed7a19b0882806bbfa28f7f436c817
riccardoFicarra <riccardo.ficarra95@gmail.com>
2019-04-17 19:52:53 -0500

    added function to print graph

--- src/bpigraph.jl
+++ src/bpigraph.jl
@@ -36,3 +36,42 @@ function valueVectors2D(controller::Controller)
     #@deb("$alphas")
     plot(0:1, alphas, label=reshape(collect(keys(controller.nodes)), 1, n_nodes))
 end
+
+using LightGraphs, GraphPlot
+function draw_controller(controller::Controller{A, W}) where {A, W}
+    nodes = controller.nodes
+    g = SimpleDiGraph(length(nodes))
+    edgelabel = Vector{String}()
+    temp_id = Dict{Int64, Int64}()
+    for i in keys(controller.nodes)
+        temp_id[i] = length(temp_id)+1
+    end
+    @deb("$temp_id")
+    for (id, node) in nodes
+        for (action, action_dict) in node.edges
+            for (obs, obs_dict) in action_dict
+                for (next, prob) in obs_dict
+                     ok = LightGraphs.add_edge!(g, temp_id[id], temp_id[next.id])
+                     #set_props!(mg, Edge(id, next.id), Dict(:action => action, :obs => obs, :prob => prob))
+                     if !ok
+                         @deb("failed $id -> $(next.id) $action $obs")
+                     else
+                         @deb("$id -> $(next.id)")
+                         push!(edgelabel, "$action $obs $prob")
+                     end
+                end
+            end
+        end
+    end
+
+    nodelabel = collect(keys(nodes))
+    #=
+    nodelabel = zeros(Int64, controller.maxId)
+    for i in 1:controller.maxId
+        if haskey(nodes, i)
+            nodelabel[i] = i
+        end
+    end
+    =#
+    gplot(g, nodelabel = nodelabel, edgelabel = edgelabel)
+end
